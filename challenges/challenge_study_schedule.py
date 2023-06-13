dfaad099faaab775f9c8677ee6121220271d6fed
felipe <felipegouveia.bfr@hotmail.com>
2023-06-13 17:01:58 -0300

    req studendt e palindromos

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int) or target_time == 0:
+        return None
+    count_student = 0
+    for period in permanence_period:
+        start, end_time = period
+        if not isinstance(start, int) or not isinstance(end_time, int):
+            return None
+        if start <= target_time <= end_time:
+            count_student += 1
+    return count_student
